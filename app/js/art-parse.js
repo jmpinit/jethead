845bc97b1340db69a4d7c11d6f83a44ec8345683
Owen Trueblood <jmptable.mit.edu>
2016-10-29 16:14:56 -0400

    More script compatibility

--- app/js/art-parse.js
+++ app/js/art-parse.js
@@ -17,21 +17,26 @@ function wait(line) {
 
 // Loc: (1026, 330) Pr: 1 Ti: 1 Ro: 1.25 Fw: 1 Bt: 0
 function loc(line) {
+    // TODO refactor
     // don't know what Fw and Bt are so they are not parsed
-    const rPos = /\((\d+(\.\d+)?),\s+(\d+(\.\d+)?)\)/;
+    const rPos = /\((-?\d+(\.\d+)?),\s+(-?\d+(\.\d+)?)\)/;
     const rPressure = /Pr: (\d+(\.\d+)?)/;
-    const rTilt = /Ti: (\d+(\.\d+)?)/;
-    const rRotation = /Ro: (\d+(\.\d+)?)/;
+    const rTilt = /Ti: (-?\d+(\.\d+)?)/;
+    const rRotation = /Ro: (-?\d+(\.\d+)?)/;
+    const rScale = /Scale: (-?\d+(\.\d+)?)/;
+    const rOff = /Off: (-?\d+(\.\d+)?)/;
+    const rSize = /Off: (-?\d+(\.\d+)?)/;
 
     const posMatches = line.match(rPos);
     const pressureMatches = line.match(rPressure);
     const tiltMatches = line.match(rTilt);
     const rotationMatches = line.match(rRotation);
+    const scaleMatches = line.match(rScale);
+    const offMatches = line.match(rOff);
+    const sizeMatches = line.match(rSize);
 
-    if (posMatches === null ||
-            pressureMatches === null ||
-            tiltMatches === null ||
-            rotationMatches === null) {
+    if (posMatches === null) {
+        console.log(posMatches, pressureMatches, tiltMatches, rotationMatches);
         throw new Error(`Not a location line: ${line}`);
     }
 
@@ -41,9 +46,12 @@ function loc(line) {
             x: parseFloat(posMatches[1]),
             y: parseFloat(posMatches[3]),
         },
-        pressure: parseFloat(pressureMatches[1]),
-        tilt: parseFloat(tiltMatches[1]),
-        rotation: parseFloat(rotationMatches[1]),
+        pressure: pressureMatches ? parseFloat(pressureMatches[1]) : undefined,
+        tilt: tiltMatches ? parseFloat(tiltMatches[1]) : undefined,
+        rotation: rotationMatches ? parseFloat(rotationMatches[1]) : undefined,
+        scale: scaleMatches ? parseFloat(scaleMatches[1]) : undefined,
+        offset: offMatches ? parseFloat(offMatches[1]) : undefined,
+        size: sizeMatches ? parseFloat(sizeMatches[1]) : undefined,
     };
 }
 
